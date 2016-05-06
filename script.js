2c7ba259fe5b948aa82abcd4b71a9b6c59cfd2a2
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2016-05-06 16:38:25 -0400

    added styling and total

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 var app = angular.module('myModule', []);
 
-app.controller('toDoController', function ($scope) {
+app.controller('toDoController', function($scope) {
 	$scope.toDo = [
 		{ item: "Read ng-book", done: false },
-		{ item: "Make to-do list app", done: false },
+		{ item: "Make to-do list application", done: false },
 		{ item: "Go grocery shopping", done: false }
 	];
+
+	$scope.addToDo = function() {
+		$scope.toDo.push({ item: $scope.formToDo, done: false });
+		$scope.formToDo = "";
+	};
+
+	$scope.total = function() {
+		return $scope.toDo.length;
+	}
+
+	// in-progress
+	$scope.clearCompleted = function() {
+        $scope.toDo.done = toDo.filter($scope.toDo, function(task){
+            return !toDo.done;
+        });
+    };
 });
